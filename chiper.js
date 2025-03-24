04f612f5a89387ac3d7002ef0a36af34196f4aa2
Ethan7862 <kusasirakwe.ethan.upti@gmail.com>
2025-03-24 16:47:37 +0300

    add encryption func

--- chiper.js
+++ chiper.js
@@ -1,1 +1,15 @@
-const alphabate = 'abcdefghijklmnopqrstuvwxyz'
+const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt = (letter, value) => {
+    const shift = (alphabets.indexOf(letter) + value) % alphabets.length;
+    return alphabets[shift];
+}
+
+
+const encryptMessage = (word, value) => {
+    let result = '';
+    for (let i = 0; i < word.length; i++) {
+        const encryptValue = encrypt(word[i], value);
+        result += encryptValue;
+    }
+    return result;
+}
