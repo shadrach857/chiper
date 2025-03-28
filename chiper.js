2bb02ed514e5f3f2651bcbb15b25a953e642d6e2
Nanvule Faith <nanvule.faith@upti.com>
2025-03-28 15:03:01 +0300

    random letter function added

--- chiper.js
+++ chiper.js
@@ -1,35 +1,47 @@
-const alphabets1 = 'abcdefghijklmnopqrstuvwxyz';
+
+
+// Function to shift letters
 const encrypt = (letter, value) => {
-    const shift = (alphabets.indexOf(letter) + value) % alphabets.length;
+    const index = alphabets.indexOf(letter);
+    if (index === -1) return letter; // Keep non-alphabet characters unchanged
+    const shift = (index + value) % alphabets.length;
     return alphabets[shift];
-}
-
-
-const encryptMessage = (word, value) => {
-    let result = '';
-    for (let i = 0; i < word.length; i++) {
-        const encryptValue = encrypt(word[i], value);
-        result += encryptValue;
-    }
-    return result;
-}
-
-const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+};
 
 const decrypt = (letter, value) => {
-    const shift = (alphabets.indexOf(letter) - value + alphabets.length) % alphabets.length;
+    const index = alphabets.indexOf(letter);
+    if (index === -1) return letter;
+    const shift = (index - value + alphabets.length) % alphabets.length;
     return alphabets[shift];
+};
+
+// Function to get a random letter
+function getRandomLetter() {
+    return alphabets[Math.floor(Math.random() * alphabets.length)];
 }
 
-const decryptMessage = (word, value) => {
+// Encrypt Message (with removable random letters)
+function encryptMessage(word, value) {
     let result = '';
     for (let i = 0; i < word.length; i++) {
-        const decryptValue = decrypt(word[i], value);  
-        result += decryptValue;   
+        result += encrypt(word[i], value); // Encrypt the letter
+        result += getRandomLetter(); // Add a random lettergit
     }
     return result;
 }
 
- 
-console.log(decryptMessage("dpef", 1));   
+// Decrypt Message (removing the random letters)
+function decryptMessage(word, value) {
+    let result = '';
+    for (let i = 0; i < word.length; i += 2) { // Skip every second character
+        result += decrypt(word[i], value);
+    }
+    return result;
+}
 
+// Example Usage
+const encrypted = encryptMessage("code", 1);
+console.log("Encrypted Message:", encrypted);
+
+const decrypted = decryptMessage(encrypted, 1);
+console.log("Decrypted Message:", decrypted);
