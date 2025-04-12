2125b5b0713794faa31baf51cf37aa1c467cb555
Ethan7628 <kusasirakwe.ethan.upti@gmail.com>
2025-04-13 00:48:46 +0300

    Removed unused cache components

--- chiper.js
+++ chiper.js
@@ -1,69 +1,17 @@
-
-// const alphabets =abcdefghijklmnopqrstuvwxyz
-
-// // Function to shift letters
-// const encrypt = (letter, value) => {
-//     const index = alphabets.indexOf(letter);
-//     if (index === -1) return letter; // Keep non-alphabet characters unchanged
-//     const shift = (index + value) % alphabets.length;
-//     return alphabets[shift];
-// };
-
-// const decrypt = (letter, value) => {
-//     const index = alphabets.indexOf(letter);
-//     if (index === -1) return letter;
-//     const shift = (index - value + alphabets.length) % alphabets.length;
-//     return alphabets[shift];
-// };
-
-// // Function to get a random letter
-// function getRandomLetter() {
-//     return alphabets[Math.floor(Math.random() * alphabets.length)];
-// }
-
-// // Encrypt Message (with removable random letters)
-// function encryptMessage(word, value) {
-//     let result = '';
-//     for (let i = 0; i < word.length; i++) {
-//         result += encrypt(word[i], value); // Encrypt the letter
-//         result += getRandomLetter(); // Add a random lettergit
-//     }
-//     return result;
-// }
-
-// // Decrypt Message (removing the random letters)
-// function decryptMessage(word, value) {
-//     let result = '';
-//     for (let i = 0; i < word.length; i += 2) { // Skip every second character
-//         result += decrypt(word[i], value);
-//     }
-//     return result;
-// }
-
-// // Example Usage
-// const encrypted = encryptMessage("code", 1);
-// console.log("Encrypted Message:", encrypted);
-
-// const decrypted = decryptMessage(encrypted, 1);
-// console.log("Decrypted Message:", decrypted);
-
-
-// Define the alphabet
-
 const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 // Function to shift letters
 const encrypt = (letter, value) => {
     const index = alphabets.indexOf(letter);
-    if (index === -1) return letter; // Keep non-alphabet characters unchanged
-    const shift = (index + (value % alphabets.length)) % alphabets.length; // Use modulo to handle large shifts
+    if (index === -1) return letter; 
+    const shift = (index + (value % alphabets.length)) % alphabets.length; 
     return alphabets[shift];
 };
 
 const decrypt = (letter, value) => {
     const index = alphabets.indexOf(letter);
-    if (index === -1) return letter; // Keep non-alphabet characters unchanged
-    const shift = (index - (value % alphabets.length) + alphabets.length) % alphabets.length; // Use modulo to handle large shifts
+    if (index === -1) return letter; 
+    const shift = (index - (value % alphabets.length) + alphabets.length) % alphabets.length; 
     return alphabets[shift];
 };
 
